ff0b91c5d33bac575a364cdd0bdd1b697a743cba
rapleeee <raflimaulanaaaa@gmail.com>
2025-06-03 09:57:37 +0700

    feat: update Tailwind configuration and refactor layout component by removing unused font imports

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatar.iran.liara.run'],
+  },
+  transpilePackages: ['framer-motion'],
+}
+
+module.exports = nextConfig
